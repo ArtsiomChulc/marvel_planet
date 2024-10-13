808eb8d9d61aaea8aa8882e826c442410b5e2803
ArtsiomChults <artemorsha@mail.ru>
2024-10-14 00:23:01 +0300

    add: card for character

---
+++ src/shared/components/molecules/cardCharacter/CardCharacter.tsx
@@ -0,0 +1,34 @@
+import { getSourceImg } from '../../../helpers/getSrc';
+import s from './CardCharacter.module.scss';
+import { Thumbnail } from '../../../../api/types/storiesType';
+import { useNavigate } from 'react-router-dom';
+
+type Props = {
+  description?: string
+  name: string
+  src: Thumbnail
+  id?: number
+}
+
+export const CardCharacter = ({src, name, description, id}: Props) => {
+  const navigate = useNavigate();
+
+  const getSelectCharacter = (id: number | undefined) => {
+    if (id) {
+      navigate(`/character/${id}`);
+    }
+  }
+
+  return (
+    <div className={s.cardContainer} onClick={() => getSelectCharacter(id)}>
+      <div className={s.imgBox}>
+        <img src={getSourceImg(src)} alt={name} />
+      </div>
+      <div className={s.tex_wrapper}>
+        <h3
+          className={s.name_card}>{name}</h3>
+        <p className={s.card_description}>{description || 'No description'}</p>
+      </div>
+    </div>
+  );
+};
