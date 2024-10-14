e2e76eb510cb3f1c8f4428152df181f0a1ae1df2
ArtsiomChults <artemorsha@mail.ru>
2024-10-14 23:43:52 +0300

    feat: use crumbs

--- src/app/components/layout/Layout.tsx
+++ src/app/components/layout/Layout.tsx
@@ -1,17 +1,18 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Loader } from '../../../shared/components/atoms/loader/Loader';
 import { Header } from '../../../shared/components/molecules/header/Header';
 import s from './Layout.module.scss';
+import {
+  BreadCrumbs
+} from '../../../shared/components/organisms/breadCrumbs/BreadCrumbs';
 
 export const Layout = () => {
-  const location = useLocation();
-
-  console.log(location);
   return (
     <div className={s.main_container}>
       <div className={s.linear_bc}>
         <Header />
+        <BreadCrumbs/>
         <Suspense fallback={<Loader />}>
           <Outlet />
         </Suspense>
