9ebb3f7f0bf2f3682bc062f2fcd60eb2699ee45b
ArtsiomChults <artemorsha@mail.ru>
2024-10-14 23:42:56 +0300

    add: bread crumbs

---
+++ src/shared/components/organisms/breadCrumbs/BreadCrumbs.tsx
@@ -0,0 +1,29 @@
+import s from './BreadCrumbs.module.scss';
+import { NavLink, useLocation } from 'react-router-dom';
+
+export const BreadCrumbs = () => {
+  const location = useLocation();
+  const pathName = location.pathname;
+  let currentPath = '';
+
+  const crumbs = pathName.split('/')
+    .filter(crumb => crumb !== '')
+    .map((crumb, index, array) => {
+      currentPath += `/${crumb}`;
+      const isLastCrumb = index === array.length - 1;
+      return (
+        <div className={s.crumb} key={crumb}>
+          <NavLink to={currentPath} className={isLastCrumb ? s.last_crumb : ''}>
+            {crumb}
+          </NavLink>
+        </div>
+      );
+    });
+  console.log(crumbs);
+
+  return (
+    <div className={s.crumbs}>
+      {crumbs}
+    </div>
+  );
+};
