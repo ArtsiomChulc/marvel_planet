6c6350c88a7a7295135682b4897c4bc6b2fa0756
ArtsiomChults <artemorsha@mail.ru>
2024-10-16 15:37:52 +0300

    add: footer layout

--- src/shared/components/molecules/footer/Footer.tsx
+++ src/shared/components/molecules/footer/Footer.tsx
@@ -1,9 +1,35 @@
+import logo from '../../../../assets/icons/Marvel_logo.png';
+
 import s from './Footer.module.scss';
 
 export const Footer = () => {
   return (
-    <div>
-
+    <div className={s.footer_container}>
+      <div className={s.logo_wrap}>
+        <img src={logo} alt="Logo Marvel" />
+      </div>
+      <div className={s.footer_content}>
+        <p className={s.title}>
+          This application is made using:
+        </p>
+        <div className={s.description}>
+          <span>
+          React js
+        </span>
+          <span>
+          Axios
+        </span>
+          <span>
+          SCSS
+        </span>
+          <span>
+          Typescript
+        </span>
+          <span>
+          React router dom
+        </span>
+        </div>
+      </div>
     </div>
   );
 };
