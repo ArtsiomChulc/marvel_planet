fd5c8c11ec767d38a176bb65460a8fdf6a72ec99
ArtsiomChults <artemorsha@mail.ru>
2024-10-17 17:13:36 +0300

    fix: some fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -90,7 +90,7 @@ export const Home = () => {
         <Button title={'Search'} onClick={getSearchCharacter} />
       </div>
       <div className={s.result_container}>
-        {!isLoading ? (
+        {isLoading ? (
           <PreLoader />
         ) : result && result.length === 0 ? (
           <NotFoundCharacters />
