af71e8be4a71188acb68f86090358552eff9a61e
ArtsiomChults <artemorsha@mail.ru>
2024-10-17 14:56:32 +0300

    feat: use mobile menu

--- src/shared/components/atoms/menuItem/component/MenuItem.tsx
+++ src/shared/components/atoms/menuItem/component/MenuItem.tsx
@@ -1,20 +1,10 @@
-import { NavLink } from 'react-router-dom';
-import s from './MenuItem.module.scss';
+import { getMenuTitle } from '../../../../helpers/getMenuTitle';
+import { menuItemsText } from '../../../molecules/header/schemas';
 
-type Props = {
-  path: string;
-  title: string;
-};
-
-export const MenuItem = ({ path, title }: Props) => {
+export const MenuItem = () => {
   return (
-    <li className={s.menu_item}>
-      <NavLink
-        className={({ isActive }) => (isActive ? s.active : '')}
-        to={path}
-      >
-        {title}
-      </NavLink>
-    </li>
+      <>
+        {getMenuTitle(menuItemsText)}
+      </>
   );
 };
--- src/shared/components/molecules/header/Header.tsx
+++ src/shared/components/molecules/header/Header.tsx
@@ -1,12 +1,32 @@
-import { MenuItems } from '../../atoms/menuItem/MenuItems';
+import { MenuItemsDesktop } from '../../atoms/menuItem/MenuItemsDesktop';
 import s from './Header.module.scss';
 import { HeaderIcon } from '../../atoms/headerIcon/HeaderIcon';
+import { Burger } from '../../atoms/burger/Burger';
+import { useState } from 'react';
+import { MenuItemsMobile } from '../../atoms/menuItem/MenuItemsMobile';
 
 export const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const openMenu = () => {
+    setOpen(true);
+  }
+
+  const closeMenu = () => {
+    setOpen(false);
+  }
+
+
   return (
     <div className={s.header_container}>
-      <HeaderIcon/>
-      <MenuItems />
+      <HeaderIcon />
+      <MenuItemsDesktop />
+
+      <Burger onClick={openMenu}/>
+
+      <MenuItemsMobile onClose={closeMenu} isOpen={isOpen}/>
+
+      {isOpen && <div className={s.blur}></div>}
     </div>
   );
 };
