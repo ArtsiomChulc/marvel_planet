e384ea84bb9e59bb6bc126d27ae81abed3f15a8b
ArtsiomChults <artemorsha@mail.ru>
2024-10-17 15:27:21 +0300

    feat: use hook click outside

--- src/shared/components/atoms/menuItem/MenuItemsMobile.tsx
+++ src/shared/components/atoms/menuItem/MenuItemsMobile.tsx
@@ -1,16 +1,37 @@
 import s from './MenuItems.module.scss';
 import { MenuItem } from './component/MenuItem';
+import { useEffect, useRef } from 'react';
+import useClickOutside from '../../../helpers/hooks/useClickOutside';
+import { useLocation } from 'react-router-dom';
 
 type Props = {
-  onClose: () => void;
   isOpen: boolean;
+  setOpen: (value: boolean) => void
 }
 
-export const MenuItemsMobile = ({onClose, isOpen}: Props) => {
+export const MenuItemsMobile = ({isOpen, setOpen}: Props) => {
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const location = useLocation();
+
+  useClickOutside(menuRef, () => setOpen(false))
+
+  useEffect(() => {
+    if(location) {
+      setOpen(false)
+    }
+  }, [location, setOpen])
+
+  const closeMenu = () => {
+    setOpen(false);
+  }
+
   return (
-    <div className={`${s.mobile_menu_container} ${isOpen ? s.open_menu : ''}`}>
-      <div className={s.close_btn_wrap}>
-        <span onClick={onClose}>X</span>
+    <div ref={menuRef} className={`${s.mobile_menu_container} ${isOpen ? s.open_menu : ''}`}>
+      <div className={s.close_btn_wrap} onClick={closeMenu}>
+        <span></span>
+        <span></span>
       </div>
       <ul className={s.menu_item_mobile}>
         <MenuItem />
