c18ad91e49b8a27b9ddb7eb36ecbf379aed3b995
ArtsiomChults <artemorsha@mail.ru>
2024-10-19 17:45:00 +0300

    fix: style preloader

--- src/shared/components/atoms/preLoader/PreLoader.tsx
+++ src/shared/components/atoms/preLoader/PreLoader.tsx
@@ -1,9 +1,14 @@
 import s from './PreLoader.module.scss';
 
 export const PreLoader = () => {
+  const line_loader = Array.from({ length: 10 }).map((_, i) => {
+    return (
+      <div key={i} className={s.loader_line}></div>
+    )
+  });
   return (
     <div className={s.loader}>
-      <div className={s.loadingBar}></div>
+      {line_loader}
     </div>
   );
 };
